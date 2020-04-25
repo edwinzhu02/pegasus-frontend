ef25dd5b156be6cf6ceadb346e28f5ae70f3d00e
edwin zhu <edwin.zhu02@gmail.com>
2020-04-25 19:43:46 +1200

    environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   // baseUrl: 'http://45.76.123.59:5000/api/',
   // photoUrl: 'http://gradspace.org:5000/',  
-   baseUrlForChatting: 'http://45.76.123.59:5000/',
-   baseUrl: 'http://localhost:5000/api/',
-   photoUrl: 'http://localhost:5000/',  
+   baseUrlForChatting: 'http://45.76.123.59:5002/',
+   baseUrl: 'http://localhost:5002/api/',
+   photoUrl: 'http://localhost:5002/',  
   // baseUrl: 'http://192.168.178.76:5000/api/',
   companyName: 'Amber Studio',
   siteName: 'Project Pegasus'
