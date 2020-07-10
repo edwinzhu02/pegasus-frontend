e05f99c96d3b541e0f850d86ec338f6c3fb84cfe
Nay-said <colin-weber@outlook.com>
2020-07-10 21:35:39 +1200

    lessons report working

--- src/app/components/dashboard/dashboard-components/admin-lessonStat/lessons-report/lessons-report.component.ts
+++ src/app/components/dashboard/dashboard-components/admin-lessonStat/lessons-report/lessons-report.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SessionsService } from 'src/app/services/http/sessions.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-lessons-report',
@@ -7,6 +8,7 @@ import { SessionsService } from 'src/app/services/http/sessions.service';
   styleUrls: ['./lessons-report.component.css']
 })
 export class LessonsReportComponent implements OnInit {
+  ifShowReport: boolean = false
   lessonsData: any;
   terms: any;
   orgs: any;
@@ -23,15 +25,13 @@ export class LessonsReportComponent implements OnInit {
     this.getLessons();
     this.getTerms();
     this.getOrgs();
-    console.log('daysOfWeek', this.daysOfWeek)
-    this.daysOfWeek.forEach(element => {
-      this.sortedData[element] = []
-    });
-    console.log('before sort', this.sortedData)
+    // console.log('before sortting', this.sortedData)
   }
 
   showReport(data) {
     console.log(data)
+    this.ifShowReport = true
+    this.sortData(this.terms[data.termSelect])
   }
 
   getLessons() {
@@ -39,12 +39,6 @@ export class LessonsReportComponent implements OnInit {
       res => {
         this.lessonsData = res['Data'];
         console.log('lessonData', this.lessonsData)
-        // Sortting Data
-        this.lessonsData.forEach(element => {
-          let day = this.getDay(element.DayOfWeek)
-          this.sortedData[day].push(element)
-        })
-        console.log('Sorted Data', this.sortedData)
       },
       err => {
         console.log(err)
@@ -56,6 +50,12 @@ export class LessonsReportComponent implements OnInit {
       res => {
         this.terms = res['Data'];
         console.log('terms', this.terms)
+        this.terms.forEach(term => {
+          this.sortedData[term.TermId-1] = []
+          this.daysOfWeek.forEach(day => {
+            this.sortedData[term.TermId-1][day] = []
+          });
+        })
       },
       err => {
         console.log(err)
@@ -63,23 +63,42 @@ export class LessonsReportComponent implements OnInit {
   }
 
   getOrgs() {
-    let userOrgs = JSON.parse(localStorage.getItem('OrgId'))
-    // console.log(userOrgs);
     this.sessionsService.getOrgs().subscribe(
       res => {
         this.orgs = res['Data']
-        console.log(this.orgs)
+        // console.log(this.orgs)
       },
       err => {
         console.log(err)
       })
-    if (Object.keys(userOrgs).length > 1) {
-      
-    }
+  }
+
+  sortData(term) {
+    console.log('Selected term', term)
+    this.lessonsData.forEach(course => {
+      if (this.match(course, term) == true) {
+        let day = this.getDay(course.DayOfWeek)
+        this.sortedData[term.TermId-1][day].push(course)
+      }
+    })
+    console.log('sorted data', this.sortedData)
+  }
+
+  match(course, term) {
+    let isMatch: boolean = false
+    course.LessonsViewModel.forEach(element => {
+      let courseDate = moment(element.OriginalDate).format( 'YYYY-MM-DD')
+      let termBegin = moment(term.BeginDate).format('YYYY-MM-DD')
+      let termEnd = moment(term.EndDate).format('YYYY-MM-DD')
+      // console.log(courseDate)
+      if (moment(courseDate).isBetween(termBegin, termEnd)) {
+        isMatch = true
+      }
+    })
+    return isMatch
   }
 
   getDay(day): string {
-    console.log(this.daysOfWeek[day])
     return this.daysOfWeek[day]
   }
 }
